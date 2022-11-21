172ef8b828f2de6564c1be0ab651390083587440
Vitaliy Kashaedov <Vitalickasahedov@mail.ru>
2022-11-21 22:17:40 +0300

    1

---
+++ seminar_1.py
@@ -0,0 +1,65 @@
+# num_a = 5
+# num_b = 7
+# num_c = 7.5
+# num_d = num_a + num_b // num_c
+# print ( type(num_d))
+# num_a = "5"
+# num_b = '7'
+# num_d = num_a + num_b
+# print ( num_d)
+# num_a = "5"
+# num_b = '7'
+# num_d = num_a * 7
+# print ( num_d)
+# num_a = "5"
+# num_b = '7'
+# num_d = num_a * 7
+# print ( num_d) # вывод
+# a = int(input('Enter number: '))
+# print(a,"asdfsdfsd",  sep='@@@', end='_____' )#sep разделение текста; end в конце текста
+# b = int(a)
+# num_1 = 1
+# num_2 = 3
+# if num_1 > num_2:
+#     print(1)
+# elif num_2 > num_1:
+#     print(2)
+# else:
+#     print(3)
+# 1. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+#      - 5, 25 -> да
+#     - 4, 16 -> да
+#     - 25, 5 -> да
+#     - 8,9 -> нет
+#2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+# Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
+# num = int(input('write number: '))
+# x = -num
+# if num < 0:
+#     while x != num - 1:
+#         print(x)
+#         x -= 1
+# else:
+#     while x != num
+#         print(x)
+#         x += 1
+#Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа.
+# num = float(input('write number: '))
+# print(int(num * 10) % 10)
+# number = float (input())
+# if int(number*10%10)  != 0:
+#     print (int(number*10%10))
+# else:
+#     print ('нет')
+#Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30.
+ # numb = int(input())
+ # if ((numb%5==0) and (numb%10==0) or (numb%15==0)) and (numb%30 != 0):
+ #     print ('кратно')
+ # else:
+ #     print ('нет')
+
+
+
+
+
+
